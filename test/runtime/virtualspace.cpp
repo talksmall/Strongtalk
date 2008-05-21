d7ebbe23a77e8398c516cb1e423a053aa4050581
StephenLRees <StephenLRees@0765cc13-ff1d-0410-8584-d7a1c31ec48e>
2008-05-21 11:03:31 +0000

    Forgot to add a file
    git-svn-id: http://strongtalk.googlecode.com/svn/branches/gcc-linux@135 0765cc13-ff1d-0410-8584-d7a1c31ec48e

---
+++ test/runtime/virtualspace.cpp
@@ -0,0 +1,33 @@
+# include "incls/_precompiled.incl"
+# include "incls/_virtualspace.cpp.incl"
+#include "easyunit/test.h"
+
+using namespace easyunit;
+
+DECLARE(ReservedSpace)
+void checkAlignment(int size, int expected_alignment) {
+  char message[100];
+  int adjustment = ReservedSpace::page_align_size(size);
+  sprintf(message, "adjustment for %d: %d", size, adjustment);
+  ASSERT_EQUALS_M(expected_alignment, adjustment, message);
+}
+END_DECLARE
+
+SETUP(ReservedSpace) {}
+
+TEARDOWN(ReservedSpace){}
+
+TESTF(ReservedSpace, page_align_size)
+{
+  // at least one page
+  checkAlignment(0, os::vm_page_size());
+  checkAlignment(1, os::vm_page_size());
+  checkAlignment(os::vm_page_size() - 1, os::vm_page_size());
+  checkAlignment(os::vm_page_size(), os::vm_page_size());
+
+  // in chunks of one page in size
+  checkAlignment(os::vm_page_size() + 1, 2 * os::vm_page_size());
+  checkAlignment(2 * os::vm_page_size() - 1, 2 * os::vm_page_size());
+  checkAlignment(2 * os::vm_page_size(), 2 * os::vm_page_size());
+  checkAlignment(2 * os::vm_page_size() + 1, 3 * os::vm_page_size());
+}
