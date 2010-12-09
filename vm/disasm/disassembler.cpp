fde63e1c1f411dab059fcd8a956dd2f42af80303
Artur Lojewski <lojewski@gmail.com>
2010-12-09 17:35:49 +0100

    replaced tohex() implementation

--- vm/disasm/disassembler.cpp
+++ vm/disasm/disassembler.cpp
@@ -56,13 +56,9 @@ static void initialize(void) {
 }
 
 static char tohex(unsigned char c) {
-  switch (c) {
-    case 0x0: return '0'; case 0x1: return '1'; case 0x2: return '2'; case 0x3: return '3';
-    case 0x4: return '4'; case 0x5: return '5'; case 0x6: return '6'; case 0x7: return '7';
-    case 0x8: return '8'; case 0x9: return '9'; case 0xA: return 'A'; case 0xB: return 'B';
-    case 0xC: return 'C'; case 0xD: return 'D'; case 0xE: return 'E'; case 0xF: return 'F';
-    default:  return '?';
-  }
+  char* digits="0123456789ABCDEF";
+  if (c > 0xf) return '?';
+  return digits[c];
 }
 
 static void disasm(char* begin, char* end, outputStream* st) {
